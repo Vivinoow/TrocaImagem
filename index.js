de921b418d3c4fd24ec11c173177ccbe4b0fa7a9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-03 22:27:13 -0300

    Atividade

---
+++ index.js
@@ -0,0 +1,12 @@
+var botao = document.getElementById('clique');
+botao.onclick = function(){
+    if(botao.value === 'troca'){
+        pers1.src = 'download.jpg';
+        botao.value = 'Retornar';
+        botao.innerHTML = '(Voltar)';
+    }else{
+        pers2.src = 'aaa.jpg';
+        botao.value = 'troca';
+        botao.innerHTML = '(Trocar)';
+    }
+}
