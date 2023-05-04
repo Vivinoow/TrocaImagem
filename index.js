b32b48153a6362cc4c849965daf91d524afdc227
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-04 19:35:44 -0300

    Atividade

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var botao = document.getElementById('clique');
 botao.onclick = function(){
     if(botao.value === 'troca'){
-        pers1.src = 'download.jpg';
+        pers1.src = 'aaa.jpg';
         botao.value = 'Retornar';
         botao.innerHTML = '(Voltar)';
     }else{
-        pers2.src = 'aaa.jpg';
+        pers1.src = 'download.jpg';
         botao.value = 'troca';
         botao.innerHTML = '(Trocar)';
     }
